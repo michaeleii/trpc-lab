950825352f8d88208d61bee35d9cdcee76d157a3
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-03-27 10:49:45 -0700

    Change daisy ui theme

--- t3practice/src/pages/_document.tsx
+++ t3practice/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme="night">
+    <Html data-theme="forest">
       <Head />
       <body>
         <Main />
